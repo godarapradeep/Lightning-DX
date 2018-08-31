0ea0dc0c9693b9863385413ccb808a331c1d20e7
Victor <vicabraham@deloitte.com>
2018-08-31 17:37:20 -0600

    Server call and data rendering

---
+++ force-app/main/default/aura/UserDetails/UserDetailsHelper.js
@@ -0,0 +1,29 @@
+({
+    getUserInfo : function(component) {
+        var action = component.get("c.getUserDetails");
+        // Create a callback that is executed after 
+        // the server-side action returns
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                console.log("From server: ", response.getReturnValue());
+                component.set("v.user",response.getReturnValue());
+            }
+            else if (state === "INCOMPLETE") {
+                // do something
+            }
+            else if (state === "ERROR") {
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " + 
+                                 errors[0].message);
+                    }
+                } else {
+                    console.log("Unknown error");
+                }
+            }
+        });
+        $A.enqueueAction(action);
+    }
+})
